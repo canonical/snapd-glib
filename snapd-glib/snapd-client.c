4231891b468ffc24bd0f6d0ce858980effe5f75c
Robert Ancell <robert.ancell@canonical.com>
2016-08-02 10:49:07 +1200

    Can connect to snapd

--- snapd-glib/snapd-client.c
+++ snapd-glib/snapd-client.c
@@ -1,18 +1,60 @@
 #include "config.h"
 
+#include <gio/gunixsocketaddress.h>
+
 #include "snapd-client.h"
 
 typedef struct
 {
-  int dummy;
+  GSocket *snapd_socket;
 } SnapdClientPrivate;
  
 G_DEFINE_TYPE_WITH_PRIVATE (SnapdClient, snapd_client, G_TYPE_OBJECT)
 
+G_DEFINE_QUARK (snapd-client-error-quark, snapd_client_error)
+
+// snapd API documentation is at https://github.com/snapcore/snapd/blob/master/docs/rest.md
+
+#define SNAPD_SOCKET "/run/snapd.socket"
+
+gboolean
+snapd_client_connect_sync (SnapdClient *client, GCancellable *cancellable, GError **error)
+{
+    SnapdClientPrivate *priv = snapd_client_get_instance_private (client);
+    g_autoptr(GSocketAddress) address = NULL;
+    g_autoptr(GError) error_local = NULL;
+
+    g_return_val_if_fail (SNAPD_IS_CLIENT (client), FALSE);  
+    g_return_val_if_fail (priv->snapd_socket == NULL, FALSE);
+
+    priv->snapd_socket = g_socket_new (G_SOCKET_FAMILY_UNIX,
+                                       G_SOCKET_TYPE_STREAM,
+                                       G_SOCKET_PROTOCOL_DEFAULT,
+                                       &error_local);
+    if (priv->snapd_socket == NULL) {
+        g_set_error (error,
+                     SNAPD_CLIENT_ERROR,
+                     SNAPD_CLIENT_ERROR_CONNECTION_FAILED,
+                     "Unable to open snapd socket: %s",
+                     error_local->message);
+        return FALSE;
+    }
+    address = g_unix_socket_address_new (SNAPD_SOCKET);
+    if (!g_socket_connect (priv->snapd_socket, address, cancellable, &error_local)) {
+        g_set_error (error,
+                     SNAPD_CLIENT_ERROR,
+                     SNAPD_CLIENT_ERROR_CONNECTION_FAILED,
+                     "Unable to connect snapd socket: %s",
+                     error_local->message);
+        g_clear_object (&priv->snapd_socket);
+        return FALSE;
+    }
+}
+
 static void
 snapd_client_class_init (SnapdClientClass *klass)
 {
-  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 }
 
 static void
--- snapd-glib/snapd-client.h
+++ snapd-glib/snapd-client.h
@@ -6,42 +6,40 @@
 #endif
 
 #include <glib-object.h>
+#include <gio/gio.h>
 
 G_BEGIN_DECLS
 
 #define SNAPD_TYPE_CLIENT             (snapd_client_get_type ())
-#define SNAPD_CLIENT(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), SNAPD_TYPE_CLIENT, SnapdClient))
-#define SNAPD_IS_CLIENT(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SNAPD_TYPE_CLIENT))
-#define SNAPD_CLIENT_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), SNAPD_TYPE_CLIENT, SnapdClientClass))
-#define SNAPD_IS_CLIENT_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), SNAPD_TYPE_CLIENT))
-#define SNAPD_CLIENT_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), SNAPD_TYPE_CLIENT, SnapdClientClass))
 
-typedef struct _SnapdClient           SnapdClient;
-typedef struct _SnapdClientClass      SnapdClientClass;
-
-struct _SnapdClient
-{
-  /*< private >*/
-  GObject parent_instance;
-};
+G_DECLARE_DERIVABLE_TYPE (SnapdClient, snapd_client, SNAPD, CLIENT, GObject)
 
 struct _SnapdClientClass
 {
-  /*< private >*/
-  GObjectClass parent_class;
+    /*< private >*/
+    GObjectClass parent_class;
 
-  /* padding, for future expansion */
-  void (* _snapd_reserved1) (void);
-  void (* _snapd_reserved2) (void);
-  void (* _snapd_reserved3) (void);
-  void (* _snapd_reserved4) (void);
+    /* padding, for future expansion */
+    void (* _snapd_reserved1) (void);
+    void (* _snapd_reserved2) (void);
+    void (* _snapd_reserved3) (void);
+    void (* _snapd_reserved4) (void);
 };
 
-GType snapd_client_get_type (void) G_GNUC_CONST;
+typedef enum {  
+    SNAPD_CLIENT_ERROR_CONNECTION_FAILED,
+    SNAPD_CLIENT_ERROR_LAST
+} SnapdClientError;
 
-SnapdClient *snapd_client_new (void);
+#define SNAPD_CLIENT_ERROR snapd_client_error_quark ()
 
-void snapd_client_connect_sync (SnapdClient *client);
+GQuark snapd_client_error_quark    (void) G_GNUC_CONST;
+
+SnapdClient *snapd_client_new      (void);
+
+gboolean snapd_client_connect_sync (SnapdClient          *client,
+                                    GCancellable         *cancellable,
+                                    GError              **error);
 
 G_END_DECLS
 
