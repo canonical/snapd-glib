137d9c4ae6abb9a48a43da986c3762b3d039fa46
Robert Ancell <robert.ancell@canonical.com>
2016-08-22 21:03:38 +1200

    Add some copyright headers

--- snapd-glib/snapd-connection.h
+++ snapd-glib/snapd-connection.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2016 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 or version 3 of the License.
+ * See http://www.gnu.org/copyleft/lgpl.html the full text of the license.
+ */
+
 #ifndef __SNAPD_CONNECTION_H__
 #define __SNAPD_CONNECTION_H__
 
