91271403e4ce4fb42b5cb2708f5bd26dcd3152f0
Robert Ancell <robert.ancell@canonical.com>
2016-10-20 18:10:22 +0200

    Some QML work...

---
+++ snapd-qt/qml-plugin.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 or version 3 of the License.
+ * See http://www.gnu.org/copyleft/lgpl.html the full text of the license.
+ */
+
+#include <QtQml/QtQml>
+#include <Snapd/Client>
+#include "qml-plugin.h"
+
+void SnapdQmlPlugin::registerTypes(const char *uri) 
+{
+    Q_ASSERT(uri == QLatin1String("Snapd"));
+    qmlRegisterType<Snapd::Client>(uri, 1, 0, "Client");
+}
---
+++ snapd-qt/qml-plugin.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 or version 3 of the License.
+ * See http://www.gnu.org/copyleft/lgpl.html the full text of the license.
+ */
+
+#ifndef QML_PLUGIN_H
+#define QML_PLUGIN_H
+
+#include <QtQml/QQmlExtensionPlugin>
+
+class SnapdQmlPlugin : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+
+public:
+    void registerTypes(const char *uri);
+};
+
+#endif
