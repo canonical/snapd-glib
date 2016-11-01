897f599661c1d83ae01a3be66a8389c12626430a
Robert Ancell <robert.ancell@canonical.com>
2016-11-01 16:23:32 +1300

    Fix names of QML types

--- snapd-qt/qml-plugin.cpp
+++ snapd-qt/qml-plugin.cpp
@@ -15,21 +15,21 @@ void SnapdQmlPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QLatin1String("Snapd"));
     qmlRegisterType<QSnapdClient>(uri, 1, 0, "SnapdClient");
-    qmlRegisterType<QSnapdAuthData>(uri, 1, 0, "AuthData");
-    qmlRegisterUncreatableType<QSnapdIcon>(uri, 1, 0, "Icon", "Can't create");
-    qmlRegisterUncreatableType<QSnapdSnap>(uri, 1, 0, "Snap", "Can't create");
-    qmlRegisterUncreatableType<QSnapdSystemInformation>(uri, 1, 0, "SystemInformation", "Can't create");
-    qmlRegisterUncreatableType<QSnapdRequest>(uri, 1, 0, "Request", "Can't create");
-    qmlRegisterUncreatableType<QSnapdConnectRequest>(uri, 1, 0, "ConnectRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdLoginRequest>(uri, 1, 0, "LoginRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdSystemInformationRequest>(uri, 1, 0, "SystemInformationRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdListRequest>(uri, 1, 0, "ListRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdListOneRequest>(uri, 1, 0, "ListOneRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdIconRequest>(uri, 1, 0, "IconRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdFindRequest>(uri, 1, 0, "FindRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdInstallRequest>(uri, 1, 0, "InstallRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdRefreshRequest>(uri, 1, 0, "RefreshRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdRemoveRequest>(uri, 1, 0, "RemoveRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdEnableRequest>(uri, 1, 0, "EnableRequest", "Can't create");
-    qmlRegisterUncreatableType<QSnapdDisableRequest>(uri, 1, 0, "DisableRequest", "Can't create");
+    qmlRegisterType<QSnapdAuthData>(uri, 1, 0, "SnapdAuthData");
+    qmlRegisterUncreatableType<QSnapdIcon>(uri, 1, 0, "SnapdIcon", "Can't create");
+    qmlRegisterUncreatableType<QSnapdSnap>(uri, 1, 0, "SnapdSnap", "Can't create");
+    qmlRegisterUncreatableType<QSnapdSystemInformation>(uri, 1, 0, "SnapdSystemInformation", "Can't create");
+    qmlRegisterUncreatableType<QSnapdRequest>(uri, 1, 0, "SnapdRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdConnectRequest>(uri, 1, 0, "SnapdConnectRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdLoginRequest>(uri, 1, 0, "SnapdLoginRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdSystemInformationRequest>(uri, 1, 0, "SnapdSystemInformationRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdListRequest>(uri, 1, 0, "SnapdListRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdListOneRequest>(uri, 1, 0, "SnapdListOneRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdIconRequest>(uri, 1, 0, "SnapdIconRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdFindRequest>(uri, 1, 0, "SnapdFindRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdInstallRequest>(uri, 1, 0, "SnapdInstallRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdRefreshRequest>(uri, 1, 0, "SnapdRefreshRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdRemoveRequest>(uri, 1, 0, "SnapdRemoveRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdEnableRequest>(uri, 1, 0, "SnapdEnableRequest", "Can't create");
+    qmlRegisterUncreatableType<QSnapdDisableRequest>(uri, 1, 0, "SnapdDisableRequest", "Can't create");
 }
