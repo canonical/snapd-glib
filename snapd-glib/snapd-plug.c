4505d24d0c3e5439da33db30c77a3d5228891b58
Robert Ancell <robert.ancell@canonical.com>
2017-01-25 21:13:07 +1300

    Fix memory leak

--- snapd-glib/snapd-plug.c
+++ snapd-glib/snapd-plug.c
@@ -271,6 +271,7 @@ snapd_plug_finalize (GObject *object)
     g_clear_pointer (&plug->name, g_free);
     g_clear_pointer (&plug->snap, g_free);
     g_clear_pointer (&plug->interface, g_free);
+    g_clear_pointer (&plug->attributes, g_hash_table_unref);
     g_clear_pointer (&plug->label, g_free);
     if (plug->connections != NULL)
         g_clear_pointer (&plug->connections, g_ptr_array_unref);
