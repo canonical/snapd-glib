24f3370fbfe08a1b93a773b08c0ea968e2d9964c
Robert Ancell <robert.ancell@canonical.com>
2017-07-17 12:29:52 +1200

    Revert "Make a C++ func const" - this breaks ABI
    This reverts commit 31f7db2cc2f4f3713a563298819bb63d755895fb.

--- snapd-qt/client.cpp
+++ snapd-qt/client.cpp
@@ -104,9 +104,10 @@ void QSnapdClient::setAuthData (QSnapdAuthData *authData)
     snapd_client_set_auth_data (d->client, SNAPD_AUTH_DATA (authData->wrappedObject ()));
 }
 
-QSnapdAuthData *QSnapdClient::authData () const
+// NOTE: This should be const but we can't do that without breaking ABI..
+QSnapdAuthData *QSnapdClient::authData ()
 {
-    Q_D(const QSnapdClient);
+    Q_D(QSnapdClient);
     return new QSnapdAuthData (snapd_client_get_auth_data (d->client));
 }
 
