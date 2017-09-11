6d4a85750007ddd4c32f209759718643e4f1f635
Robert Ancell <robert.ancell@canonical.com>
2017-09-11 17:03:33 +1200

    Add 1.22 version macro

--- snapd-glib/snapd-version.h
+++ snapd-glib/snapd-version.h
@@ -212,4 +212,13 @@
  */
 #define SNAPD_GLIB_VERSION_1_21
 
+/**
+ * SNAPD_GLIB_VERSION_1_22:
+ *
+ * A define that can be used by the C pre-processor to check for features in 1.22
+ *
+ * Since: 1.22
+ */
+#define SNAPD_GLIB_VERSION_1_22
+
 #endif /* __SNAPD_GLIB_VERSION_H__ */
