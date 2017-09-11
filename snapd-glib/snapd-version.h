9c3e2c1138bb92e3d418a4479fe33bbe2fbe6935
Robert Ancell <robert.ancell@canonical.com>
2017-09-11 16:38:47 +1200

    Releasing 1.21

--- snapd-glib/snapd-version.h
+++ snapd-glib/snapd-version.h
@@ -203,4 +203,13 @@
  */
 #define SNAPD_GLIB_VERSION_1_20
 
+/**
+ * SNAPD_GLIB_VERSION_1_21:
+ *
+ * A define that can be used by the C pre-processor to check for features in 1.21
+ *
+ * Since: 1.21
+ */
+#define SNAPD_GLIB_VERSION_1_21
+
 #endif /* __SNAPD_GLIB_VERSION_H__ */
