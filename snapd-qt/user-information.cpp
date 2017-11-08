a6ddbe1dc595abe00111cb6b903e736ca81a4a9d
Robert Ancell <robert.ancell@canonical.com>
2017-11-09 12:48:13 +1300

    Add missing file...

---
+++ snapd-qt/user-information.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 or version 3 of the License.
+ * See http://www.gnu.org/copyleft/lgpl.html the full text of the license.
+ */
+
+#include <snapd-glib/snapd-glib.h>
+
+#include "Snapd/user-information.h"
+
+QSnapdUserInformation::QSnapdUserInformation (void *snapd_object, QObject *parent) : QSnapdWrappedObject (g_object_ref (snapd_object), g_object_unref, parent) {}
+
+QString QSnapdUserInformation::username () const
+{
+    return snapd_user_information_get_username (SNAPD_USER_INFORMATION (wrapped_object));
+}
+
+QStringList QSnapdUserInformation::sshKeys () const
+{
+    gchar **ssh_keys = snapd_user_information_get_ssh_keys (SNAPD_USER_INFORMATION (wrapped_object));
+    QStringList result;
+    for (int i = 0; ssh_keys[i] != NULL; i++)
+        result.append (ssh_keys[i]);
+    return result;
+}
