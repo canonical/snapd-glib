85cc73ee1eb8bccfafa3de3e035ebfc209126921
Sergio Costas <sergio.costas@canonical.com>
2025-06-11 09:54:11 +0200

    Update definitions to version 1.71 (#214)
    Since version 1.70 has been tagged, this PR changes all the
    version definitions to 1.71, to make the code ready for the
    next release.

--- snapd-glib/snapd-version.h
+++ snapd-glib/snapd-version.h
@@ -714,4 +714,14 @@
  */
 #define SNAPD_GLIB_VERSION_1_70
 
+/**
+ * SNAPD_GLIB_VERSION_1_71:
+ *
+ * A define that can be used by the C pre-processor to check for features
+ * in 1.71
+ *
+ * Since: 1.71
+ */
+#define SNAPD_GLIB_VERSION_1_71
+
 #endif /* __SNAPD_GLIB_VERSION_H__ */
