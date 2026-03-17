8a82f62cfb7cac56b02befcfca855c403efd22ae
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-03-17 20:16:16 +0100

    build: Post-release version bump

--- snapd-glib/snapd-version.h
+++ snapd-glib/snapd-version.h
@@ -734,4 +734,14 @@
  */
 #define SNAPD_GLIB_VERSION_1_72
 
+/**
+ * SNAPD_GLIB_VERSION_1_73:
+ *
+ * A define that can be used by the C pre-processor to check for features
+ * in 1.73
+ *
+ * Since: 1.73
+ */
+#define SNAPD_GLIB_VERSION_1_73
+
 #endif /* __SNAPD_GLIB_VERSION_H__ */
